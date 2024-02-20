5ca9cd4c3868a8786c001afda18ebea71b6e4ee6
ayushm231 <ayushm231@gmail.com>
2024-02-20 15:46:15 +0530

    fix: typescript errors

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -8,8 +8,22 @@ import React from "react"
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendEmail = async (formData: FormData) => {
-  const senderEmail = formData.get("senderEmail")
-  const message = formData.get("message")
+  const senderEmailEntry = formData.get("senderEmail")
+  const messageEntry = formData.get("message")
+
+  // Ensure that the values are not null and are strings
+  if (
+    typeof senderEmailEntry !== "string" ||
+    typeof messageEntry !== "string"
+  ) {
+    return {
+      error: "Invalid form data. Sender email and message must be strings.",
+    }
+  }
+
+  // Now senderEmail and message are guaranteed to be strings
+  const senderEmail = senderEmailEntry
+  const message = messageEntry
 
   // Server Side Validation
   if (!validateString(senderEmail, 500)) {
@@ -40,6 +54,6 @@ export const sendEmail = async (formData: FormData) => {
     }
   }
   return {
-    data
+    data,
   }
 }
